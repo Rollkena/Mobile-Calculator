1234c3e255187fd0fb192277f5512372ec9e7c9a
Rollkena <rollkena@gmail.com>
2022-12-02 18:09:46 +0500

    Функции почти готовы

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.calculator
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -28,8 +30,13 @@ class MainActivity : AppCompatActivity() {
         btn_seven.setOnClickListener{setTextFields("7")}
         btn_eight.setOnClickListener{setTextFields("8")}
         btn_nine.setOnClickListener{setTextFields("9")}
-        btn_dot.setOnClickListener{setTextFields(".")}
+        btn_dot.setOnClickListener{
+            if (math_operation.text.toString().indexOf(".") == -1){
+                setTextFields(".")
+            }
+        }
 
+        //вычетание
         btn_minus.setOnClickListener{
 
             try{
@@ -44,44 +51,12 @@ class MainActivity : AppCompatActivity() {
                     first_num = result_text.text.toString()
                     math_operation.text = ""
                 }
-
-
-
-                //var text: String = math_operation.text.toString()
-                //if(!text[text.length-1].isDigit()){
-                //    math_operation.text = text.substring(0, text.length - 1);
-                //}
-                //if (first_num == "" || math_operation.text.toString().indexOf("-", 1) == -1) {
-                //    if(math_operation.text.toString().length <= 12){
-                //        first_num = math_operation.text.toString()
-                //        setTextFields("-")
-                //        Log.d("Message", "num1 = ${first_num}")
-                //    }
-                //    else if(math_operation.text.toString().length > 12){
-                //        math_operation.text = "Вы ввели слишком большое число."
-                //        result_text.text = ""
-                //        first_num = ""
-                //        second_num = ""
-                //    }
-                //}
-                //else {
-                //    var op_pos: Int = math_operation.text.toString().indexOf("-", 1)
-                //    if (first_num.substring(first_num.length - 1) != "-"){
-                //        first_num += "-"
-                //    }
-                //    second_num = math_operation.text.substring(op_pos + 1, math_operation.text.length)
-                //    Log.d("Message", "num1 = ${first_num} num2 = ${second_num}")
-                //    stringBilder(first_num, second_num)
-                //    first_num = math_operation.text.toString()
-                //    second_num = ""
-                //    setTextFields("-")
-                //    Log.d("Message", "num1 = ${first_num} num2 = ${second_num}, op_pos = ${op_pos}")
-                //}
             }
             catch (e:Exception){
                 Log.d("Error.","Сообщение: ${e.message}")
             }
         }
+        //сложение
         btn_plus.setOnClickListener{
             try {
                 operation = "+"
@@ -100,6 +75,7 @@ class MainActivity : AppCompatActivity() {
                 Log.d("Error.","Сообщение: ${e.message}")
             }
         }
+        //озведение в степень
         btn_exp.setOnClickListener{
             try {
                 operation = "^"
@@ -118,10 +94,44 @@ class MainActivity : AppCompatActivity() {
                 Log.d("Error.","Сообщение: ${e.message}")
             }
         }
-        //
-        btn_multiply.setOnClickListener{setTextFields("*")}
+        //умножение
+        btn_multiply.setOnClickListener{
+            try {
+                operation = "*"
+                if (first_num == ""){
+                    first_num = math_operation.text.toString()
+                    math_operation.text = ""
+                }
+                else{
+                    second_num = math_operation.text.toString()
+                    stringBilder(first_num, second_num, operation)
+                    first_num = result_text.text.toString()
+                    math_operation.text = ""
+                }
+            }
+            catch (e:Exception){
+                Log.d("Error.","Сообщение: ${e.message}")
+            }
+        }
         //деление
-        btn_div.setOnClickListener{setTextFields("/")}
+        btn_div.setOnClickListener{
+            try {
+                operation = "/"
+                if (first_num == ""){
+                    first_num = math_operation.text.toString()
+                    math_operation.text = ""
+                }
+                else{
+                    second_num = math_operation.text.toString()
+                    stringBilder(first_num, second_num, operation)
+                    first_num = result_text.text.toString()
+                    math_operation.text = ""
+                }
+            }
+            catch (e:Exception){
+                Log.d("Error.","Сообщение: ${e.message}")
+            }
+        }
         //кнопка равно
         btn_equal.setOnClickListener{
             try{
@@ -142,24 +152,43 @@ class MainActivity : AppCompatActivity() {
             second_num = ""
         }
     }
+    //внесение чисел
     fun setTextFields(str: String){
         math_operation.append(str)
     }
+    //операции
     fun stringBilder(str_1: String, str_2: String, str_operation: String){
         var num_1: Double = str_1.toDouble()
         var num_2: Double = str_2.toDouble()
         Log.d("Message", "num1 = ${num_1}, num2 = ${num_2}, operation = ${str_operation}")
         if (str_operation == "-"){
-            result_text.text = (num_1-num_2).toString()
+            result_text.text = (num_1 - num_2).toString()
         }
         else if(str_operation == "+"){
-            result_text.text = (num_1+num_2).toString()
+            result_text.text = (num_1 + num_2).toString()
         }
         else if(str_operation == "^"){
-            for (i in 0 until floor(num_2).toInt()) {
-                num_1 *= num_2
+            if (str_2[0] == '0'){
+                num_1 = 1.0;
+            }
+            else{
+                for (i in 0 until floor(num_2).toInt()) {
+                    num_1 *= num_2
+                }
             }
             result_text.text = (num_1).toString()
+        }
+        else if(str_operation == "*"){
+            result_text.text = (num_1 * num_2).toString()
+        }
+        else if(str_operation == "/"){
+            if (str_2[0] == '0'){
+                btn_ac.performClick()
+                result_text.text = "Ошибка"
+            }
+            else{
+                result_text.text = (num_1 / num_2).toString()
+            }
         }
     }
 }
